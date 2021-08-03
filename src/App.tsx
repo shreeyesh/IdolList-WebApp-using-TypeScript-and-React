2b3ae3706dfa11d688ef2f0527da58b3c078f400
trippdev.eth <shriyesh15@gmail.com>
2021-08-04 03:32:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,17 +17,7 @@ export interface IState {
 function App() {
 
   const [people, setPeople] = useState<IState["people"]>([
-    // {
-    //   name: "LeBron James",
-    //   age: 35,
-    //   img: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
-    //   note: "Allegeric to staying on the same team",
-    // },
-    // {
-    //   name: "Kobe Bryant",
-    //   age: 42,
-    //   img: "https://fullpresscoverage.com/wp-content/uploads/2020/01/101524695-457220551.jpg"
-    // }
+   
   ])
 
   return (
